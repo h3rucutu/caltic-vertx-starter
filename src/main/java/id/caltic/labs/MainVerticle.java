587f98c19cb403a65caa41d2eefbbf59275e5de9
Heru Pranoto <h3rucutu@gmail.com>
2018-10-06 15:01:01 +0700

    Initial commit

---
+++ src/main/java/id/caltic/labs/MainVerticle.java
@@ -0,0 +1,18 @@
+package id.caltic.labs;
+
+import io.vertx.core.AbstractVerticle;
+
+import java.util.Optional;
+
+public class MainVerticle extends AbstractVerticle {
+
+  @Override
+  public void start() {
+  	int port = Integer.valueOf(Optional.ofNullable(System.getenv("PORT")).orElse("9000"));
+
+  	vertx.createHttpServer()
+        .requestHandler(req -> req.response().end("Hello Caltic Vert.x!"))
+        .listen(port);
+  }
+
+}
